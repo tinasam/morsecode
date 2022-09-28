dc2052f5361f401445ea85b847d427619fe72ea5
Tina Samuels <mssamuelstina@gmail.com>
2022-09-28 16:33:40 -0400

    Create Pseudo code and started HTML

---
+++ morsecode.js
@@ -0,0 +1,37 @@
+const morseConvert = [
+    "A:.-",
+    "B:-...",
+    "C:-.-.",
+    "D: -..",
+    "E: .",
+    "F: ..-.",
+    "G: --.",
+    "H: ....",
+    "I: ..",
+    "J: .---",
+    "K: -.-",
+    "L: .-..",
+    "M: --",
+    "N: -.",
+    "O: ---",
+    "P: .--.",
+    "Q: --.-",
+    "R: .-.",
+    "S: ...",
+    "T: -",
+    "U: ..-",
+    "W: .--",
+    "X: -..-",
+    "Y: -.--",
+    "Z: --.."
+];
+
+// Alert to ask for user to enter TEXT only in display box
+    // 
+
+    
+// User enters  text in English
+
+// Translate text into Morse code
+
+// Display Morse code under English text in display box
