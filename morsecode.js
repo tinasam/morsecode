b802b08e1d00ce84afb75efbe7f519800817cd15
Tina Samuels <mssamuelstina@gmail.com>
2022-09-28 19:50:02 -0400

    added first function for morse code translation

--- morsecode.js
+++ morsecode.js
@@ -1,38 +1,53 @@
-const morseConvert = [
-    "A:.-",
-    "B:-...",
-    "C:-.-.",
-    "D: -..",
-    "E: .",
-    "F: ..-.",
-    "G: --.",
-    "H: ....",
-    "I: ..",
-    "J: .---",
-    "K: -.-",
-    "L: .-..",
-    "M: --",
-    "N: -.",
-    "O: ---",
-    "P: .--.",
-    "Q: --.-",
-    "R: .-.",
-    "S: ...",
-    "T: -",
-    "U: ..-",
-    "W: .--",
-    "X: -..-",
-    "Y: -.--",
-    "Z: --.."
-];
-
-// Alert to ask for user to enter TEXT only in display box
-    // 
-
+// Alert to ask for user to enter TEXT only in display box 
 
 // User enters  text in English
-    //function 
-
+   
 // Translate text into Morse code
 
 // Display Morse code under English text in display box
+
+// const morseConvert = {
+//     "A": ".-",
+//     "B": "-...",
+//     "C": "-.-.",
+//     "D": "-..",
+//     "E": ".",
+//     "F": "..-.",
+//     "G": "--.",
+//     "H": "....",
+//     "I": "..",
+//     "J": ".---",
+//     "K": "-.-",
+//     "L": ".-..",
+//     "M": "--",
+//     "N": "-.",
+//     "O": "---",
+//     "P": ".--.",
+//     "Q": "--.-",
+//     "R": ".-.",
+//     "S": "...",
+//     "T": "-",
+//     "U": "..-",
+//     "W": ".--",
+//     "X": "-..-",
+//     "Y": "-.--",
+//     "Z": "--.."
+//  }
+
+//  const morseConvert = (str) => {
+//     return str.toUpperCase().split("").map(el => {
+//        return morseCode[el] ? morseCode[el] : el;
+//     }).join("");
+//  };
+
+const convertToMorse = (str) => {
+    return str.toUpperCase().split("").map(el => {
+       return morseCode[el];
+    });
+ };
+
+ console.log(convertToMorse("Hello, My Friend!"));
+ console.log(convertToMorse('hey there!'));
+ 
+
+
