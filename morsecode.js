b82375e810cbdab5cf68d7bea132b52dbf1beb1f
Tina Samuels <mssamuelstina@gmail.com>
2022-09-28 23:15:32 -0400

    added style -translate & clear morse code buttons

--- morsecode.js
+++ morsecode.js
@@ -6,48 +6,72 @@
 
 // Display Morse code under English text in display box
 
-// const morseConvert = {
-//     "A": ".-",
-//     "B": "-...",
-//     "C": "-.-.",
-//     "D": "-..",
-//     "E": ".",
-//     "F": "..-.",
-//     "G": "--.",
-//     "H": "....",
-//     "I": "..",
-//     "J": ".---",
-//     "K": "-.-",
-//     "L": ".-..",
-//     "M": "--",
-//     "N": "-.",
-//     "O": "---",
-//     "P": ".--.",
-//     "Q": "--.-",
-//     "R": ".-.",
-//     "S": "...",
-//     "T": "-",
-//     "U": "..-",
-//     "W": ".--",
-//     "X": "-..-",
-//     "Y": "-.--",
-//     "Z": "--.."
-//  }
+const morseConvert = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--.."
+ }
 
-//  const morseConvert = (str) => {
+const entryBoxTranslator = document.querySelector(".text_entry");
+const clearMorse = document.querySelector("#clear");
+const engTranslator = document.querySelector("#translator-button");
+
+clearMorse.addEventListener("click", (event) => {
+    entryBoxTranslator.innerHTLM = " ";
+});
+
+engTranslator.addEventListener("click", (event) => {
+     const morseConversion = (str) => {
+    return str.toUpperCase().split("").map(el => {
+       return morseConvert[el] ? morseConvert[el] : el;
+    }).join("");
+ };
+});
+// const morseConversion = (str) => {
+//     return morseConvert;
+// };
+
+//  const morseConversion = (str) => {
 //     return str.toUpperCase().split("").map(el => {
-//        return morseCode[el] ? morseCode[el] : el;
+//        return morseConvert[el] ? morseConvert[el] : el;
 //     }).join("");
 //  };
 
-const convertToMorse = (str) => {
-    return str.toUpperCase().split("").map(el => {
-       return morseCode[el];
-    });
- };
-
- console.log(convertToMorse("Hello, My Friend!"));
- console.log(convertToMorse('hey there!'));
- 
+// const morseConversion = (str) => {
+//     return (el => {
+//        return morseConvert[el];
+//     // });
+//  });
+// }
 
 
+
+//  const morseConversion = (str) => {
+//     return str.toUpperCase().map(el => {
+//         return morseConvert[el];
+//     });
+//  }
+//  console.log(morseConversion("Hello My Friend"));
+//  console.log(morseConversion('hey there!'));
